990817c1ce13481fd77a65ba73faab2f1fc47752
Andrew Dymond <andy.dymond@gmail.com>
2018-02-23 09:25:16 -0700

    Add visitor navigates to trips index spec

---
+++ spec/features/vistor_sees_trips_index_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "visitor navigates to trips index" do
+  it "allows user to navigate to trip show" do
+    trip = Trip.create(name: "trip 1")
+    trail = trip.trails.create(name: "falls trail", address: "123 mountaintown st", length: 10)
+
+    visit trips_path
+    click_on trip.name
+
+    expect(current_path).to be(trip_path(trip))
+    expect(page).to have_content(trail.name)
+    expect(page).to have_content(trail.address)
+    expect(page).to have_content(trail.length)
+  end
+
+end
