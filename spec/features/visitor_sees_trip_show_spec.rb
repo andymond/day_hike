edee29e7b1b094796dca9d7b21e9e81130e315bf
Andrew Dymond <andy.dymond@gmail.com>
2018-02-23 10:00:00 -0700

    Add trails show resource, trails controller & trail show action and view

--- spec/features/visitor_sees_trip_show_spec.rb
+++ spec/features/visitor_sees_trip_show_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 describe "visitor visits trip show" do
   it "displays planned total length of trails" do
     trip = Trip.create(name: "trip 1")
-    trail = trip.trails.create(name: "falls trail", address: "123 mountaintown st", length: 10)
-    trail_2 = trip.trails.create(name: "sun trail", address: "143 mountaintown st", length: 15)
+    trip.trails.create(name: "falls trail", address: "123 mountaintown st", length: 10)
+    trip.trails.create(name: "sun trail", address: "143 mountaintown st", length: 15)
 
     visit trip_path(trip)
 
